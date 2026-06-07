0116f897e86c9dd7ccece59410f20eae21f42d12
eklochkova101 <260723543+eklochkova101@users.noreply.github.com>
2026-06-07 18:01:30 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,31 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2026 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+
+#include <vector>
+
+struct PermNode {
+  char symbol;
+  std::vector<PermNode*> branches;
+  size_t totalPaths;
+  explicit PermNode(char c) : symbol(c), totalPaths(0) {}
+};
+
+class PMTree {
+ public:
+    explicit PMTree(const std::vector<char>& in);
+    ~PMTree();
+    PermNode* head() const { return root; }
+
+ private:
+    PermNode* root;
+    void grow(PermNode* parent, const std::vector<char>& unused);
+    void del(PermNode* node);
+    size_t countPaths(PermNode* node);
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
